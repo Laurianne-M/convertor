c2636dec40fbf9f419c8f55f030ad0180184076a
laurianne <laurianne.mbanda@gmail.com>
2026-03-27 18:04:25 -0400

    api test restructuration using class

--- src/js/api/api.test.js
+++ src/js/api/api.test.js
@@ -1,167 +1,86 @@
 import { describe, vi, it, beforeEach, afterEach, expect } from "vitest";
-import { getDataFromLocalStorage, areDataOutdated, getMockRates, loadRates } from "./api.js"; 
 import { API } from "./api.js";  
 
-describe("getDataFromLocalStorage", () => {
+const fakeFetch = vi.fn(() =>
+    Promise.resolve({
+        json: () => Promise.resolve({ rates: { CAD: 1.4, USD: 1.5 }, base: "EUR" } )
+    })
+);
+
+const limitFetch = vi.fn(() =>
+    Promise.resolve({
+         json: () => Promise.resolve({ error: true, message: "monthly limit reached" })
+    })
+);
+
+const errorFetch = vi.fn() (() => Promise.reject(new Error('network failure'))); 
+
+describe('api', () => {
     let api; 
 
     beforeEach(() => {
-        api = new API(); 
-        localStorage.clear();
-    } );
-
-    it('should get the parsed data from local storage if the data exists', () => {
-        
-        const mockedData = { jsonData: { rates: { CAD:1.4, USD: 1.5 }, base: "EUR" } , receivedAt: new Date() };
-        localStorage.setItem("data", JSON.stringify(mockedData));
-
-        const data = getDataFromLocalStorage();
-
-        expect(data).toEqual(expect.objectContaining( { jsonData: { rates: { CAD:1.4, USD: 1.5 }, base: "EUR" } } ));
-
-        } );
-
-    it('should return null if there is no data in local storage', () => {
-        const mockedData = null; 
-
-        localStorage.setItem("data", JSON.stringify(mockedData));
-        
-        const data = getDataFromLocalStorage(); 
-
-        expect(data).toBe(null); 
-    } );
-} );
-
-
-describe('areDataOutdated', () => {
-    
-    beforeEach(() => {
         vi.useFakeTimers();
-    } );
+        api = new API( { fetch : fakeFetch } ); 
+        localStorage.clear();
+        vi.clearAllMocks();
+    });
 
     afterEach(() => {
         vi.useRealTimers();
-    })
-
-    it('should return true if there is no data', () => {
-        const receivedAt = null;
-
-        expect(areDataOutdated(receivedAt)).toBe(true); 
-    } );
-
+    })      
+    
     it('should return true if the timestamp is not a number', () => {
         const receivedAt = 'not a number'; 
 
-        expect(areDataOutdated(receivedAt)).toBe(true); 
-    } ); 
-
-    it('should return true if data is older than 24 hours', () => {
-        vi.setSystemTime(new Date('2026-03-22'));
-
-        const oldDate = new Date('2026-03-20').toISOString();
-
-        expect(areDataOutdated(oldDate)).toBe(true); 
-    } );
-
-    it('should return false if data is not older than 24 hours', () => {
-        vi.setSystemTime(new Date('2026-03-22'));
-
-        const today = new Date('2026-03-22').toISOString(); 
-
-        expect(areDataOutdated(today)).toBe(false); 
-    } );
-} );
-
-
-
-describe("localStorageCashingFlow", () => { 
-
-    beforeEach(() => {
-        vi.useFakeTimers();
-        vi.clearAllMocks()
-        localStorage.clear();
-    } );
-
-    afterEach(() => {
-        vi.useRealTimers();
-    } ); 
+        expect(api.areDataOutdated(receivedAt)).toBe(true); 
+    }); 
 
     it("should cached data from localStorage without fetching again", async () => {
+        await api.loadRates(); 
 
-        global.fetch = vi.fn(() =>
-            Promise.resolve({
-                json: () => Promise.resolve( { rates: { CAD: 1.4, USD: 1.5 }, base: "EUR", receivedAt: new Date() } ) 
-            } )
-        );
+        await api.loadRates(); 
 
-        await loadRates(); 
-
-        await loadRates(); 
-
-        expect(fetch).toHaveBeenCalledTimes(1); 
-
-    } );
-
-    it("should fetch and caching new data if they are outdated", async () => {
-            
-            vi.setSystemTime(new Date('2026-03-24'));
-            
-            const mockedData = { jsonData: { rates: { CAD:1.4, USD: 1.5 }, base: "EUR" } , receivedAt: new Date('2026-03-20') };
-            localStorage.setItem("data", JSON.stringify(mockedData));
-
-            await loadRates(); 
-
-            expect(fetch).toHaveBeenCalledTimes(1); 
-
-        } ); 
-} );
-
-describe('loadRates', () => {
-
-    beforeEach(() => {
-        localStorage.clear();
+        expect(fakeFetch).toHaveBeenCalledTimes(1); 
     });
 
-    it('should return the data if data exist and are not outdated', async () => {
-        const mockedData = { jsonData: { rates: { CAD:1.4, USD: 1.5 }, base: "EUR" } , receivedAt: new Date() };
+    it("should fetch and caching new data if they are outdated", async () => {  
+        vi.setSystemTime(new Date('2026-03-24'));
+        
+        const mockedData = { jsonData: { rates: { CAD:1.4, USD: 1.5 }, base: "EUR" } , receivedAt: new Date('2026-03-20') };
         localStorage.setItem("data", JSON.stringify(mockedData));
 
-        expect(await loadRates()).toEqual(expect.objectContaining( { rates: { CAD:1.4, USD: 1.5 }, base: "EUR" } ));
+        await api.loadRates(); 
+
+        expect(fakeFetch).toHaveBeenCalledTimes(1); 
+    }); 
+
+    it('should return the data if data exist and are not outdated', async () => {
+        vi.setSystemTime(new Date('2026-03-20'));
+
+        const mockedData = { jsonData: { rates: { CAD:1.9, USD: 1.6 }, base: "EUR" } , receivedAt: new Date('2026-03-20') };
+        localStorage.setItem("data", JSON.stringify(mockedData));
+
+        expect(await api.loadRates()).toEqual(expect.objectContaining( { rates: { CAD:1.9, USD: 1.6 }, base: "EUR" } ));
     } );
 
     it('it should fetch new data from the API if the data do not exist or are outdaded', async () => {
-       
-        global.fetch = vi.fn(() =>
-            Promise.resolve({
-                json: () => Promise.resolve({ rates: { CAD: 1.4, USD: 1.5 }, base: "EUR" })
-            } )
-        );
-
-        expect(await loadRates()).toEqual(expect.objectContaining( { rates: { CAD: 1.4, USD: 1.5 }, base: "EUR" } ));
+        expect(await api.loadRates()).toEqual(expect.objectContaining( { rates: { CAD: 1.4, USD: 1.5 }, base: "EUR" } ));
     } ); 
 
     it('it should return the mocked Data if data doesnt exist and the API responds with no rates (reach limit)', async () => {
 
-        global.fetch = vi.fn(() =>
-            Promise.resolve({
-                json: () => Promise.resolve({ error: true, message: "monthly limit reached" })
-            } )
-        );
+        api = new API( { fetch : limitFetch } ) 
 
-        expect(await loadRates()).toEqual(expect.objectContaining( { rates: getMockRates().rates, base: getMockRates().base } ));
+        expect(await api.loadRates()).toEqual(expect.objectContaining( { rates: api.getMockRates().rates, base: api.getMockRates().base } ));
     } );
 
     it('it should return mocked Data in case of fetch failure (network error)', async () => {
-        global.fetch = vi.fn (() => Promise.reject(new Error('network failure')))
+       api = new API( { fetch: errorFetch } );
 
-        expect(await loadRates()).toEqual(expect.objectContaining( { rates: getMockRates().rates, base: getMockRates().base } ));
+        expect(await api.loadRates()).toEqual(expect.objectContaining( { rates: api.getMockRates().rates, base: api.getMockRates().base } ));
     } );
-} );
-
-
-describe('getMockRates', () => {
 
     it('should return the mocked data', () => { 
-        expect(getMockRates()).toEqual(expect.objectContaining( { rates: getMockRates().rates, base: getMockRates().base } ));
-    } );
-} );
+            expect(api.getMockRates()).toEqual(expect.objectContaining( { rates: api.getMockRates().rates, base: api.getMockRates().base } ));
+    });
+});
