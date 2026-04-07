e5eda7ce45dee2bc2e40b68c0a6667a92c808c6e
laurianne <laurianne.mbanda@gmail.com>
2026-04-07 17:57:40 -0400

    new exclusions in vitest config file

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom', // or 'happy-dom'
-    exclude: ['tests/e2e/**'],
+    exclude: ['**/e2e/**',
+      '**/node_modules/**',
+      '**/*.spec.ts',],
   },
 })
