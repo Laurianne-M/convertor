680bf1c60f9c76fee8b92b51e10c9ef8d877190b
Laurianne Mbanda <laurianne.mbanda@gmail.com>
2026-05-12 01:04:37 +0000

    Make devcontainer runnable again

--- src/web/playwright.config.ts
+++ src/web/playwright.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
     webServer: {
-      command: 'npm run dev',
+      command: 'npm run start',
       url: 'http://localhost:5174',
       reuseExistingServer: !process.env.CI,
       timeout:  120000
