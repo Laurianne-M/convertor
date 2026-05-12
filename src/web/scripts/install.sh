fe5e4741b5b2864f3471bbf3dcf0c74ba2d45127
Jabari Dash <jabari.dash@gmail.com>
2026-05-12 02:21:16 +0000

    Optionally install Playwright system deps when run outside of GitHub Action

--- src/web/scripts/install.sh
+++ src/web/scripts/install.sh
@@ -3,4 +3,11 @@ set -e
 set -x
 
 npm ci
+
+if [ "$GITHUB_ACTIONS" != "true" ]; then
+  npx playwright install-deps
+else
+  echo "Running in GitHub Actions, skipping Playwright dependency installation."
+fi
+
 npx playwright install
