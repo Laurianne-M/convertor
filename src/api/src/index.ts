459a2474ecb7e0b599d0b0348e165daf2978013d
Laurianne Mbanda <laurianne.mbanda@gmail.com>
2026-08-08 19:52:58 +0000

    Build barebones API in Express with /health endpoint

---
+++ src/api/src/index.ts
@@ -0,0 +1,17 @@
+import express, { type Express, type Response, type Request } from 'express';
+
+const app: Express = express();
+const PORT = 3000;
+
+app.get('/health', (req: Request, res: Response) => {
+  res.status(200).json(
+    {
+      "message": "Ok",
+      "status": 200
+    }
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/health`)
+});
