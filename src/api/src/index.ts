985b24732b3fde54a73d1be7be8d3f7c57307938
Laurianne Mbanda <laurianne.mbanda@gmail.com>
2026-08-10 20:59:47 +0000

    remove port 3000,+ update node engines to match node version

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -2,7 +2,6 @@ import {onRequest} from "firebase-functions/https";
 import express, {type Express, type Response, type Request} from "express";
 
 const app: Express = express();
-const PORT = 3000;
 
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).json(
@@ -11,10 +10,6 @@ app.get("/health", (req: Request, res: Response) => {
       "status": 200,
     }
   );
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/health`);
 });
 
 export const api = onRequest(app);
