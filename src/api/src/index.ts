c6c9bd282840a73a9eedb78af38b775ffac651b7
Laurianne Mbanda <laurianne.mbanda@gmail.com>
2026-08-10 20:07:49 +0000

    firebase deploy api

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -1,17 +1,20 @@
-import express, { type Express, type Response, type Request } from 'express';
+import {onRequest} from "firebase-functions/https";
+import express, {type Express, type Response, type Request} from "express";
 
 const app: Express = express();
 const PORT = 3000;
 
-app.get('/health', (req: Request, res: Response) => {
+app.get("/health", (req: Request, res: Response) => {
   res.status(200).json(
     {
       "message": "Ok",
-      "status": 200
+      "status": 200,
     }
   );
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/health`)
+  console.log(`Server running on http://localhost:${PORT}/health`);
 });
+
+export const api = onRequest(app);
