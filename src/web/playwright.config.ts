de56835f0f9c38dc9561992d7275fc2e543eee3b
jabaridash <13109217+jabaridash@users.noreply.github.com>
2026-08-27 17:25:05 -0400

    Change web server command from npm to make

--- src/web/playwright.config.ts
+++ src/web/playwright.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
     webServer: {
-      command: 'npm run prod',
+      command: 'make prod',
       url: 'http://localhost:5174',
       reuseExistingServer: !process.env.CI,
       timeout:  120000
